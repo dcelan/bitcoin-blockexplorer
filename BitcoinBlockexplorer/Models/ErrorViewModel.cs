c5b0b27496229188377c2f5cee37d18be24c5f19
dcelan2 <56230271+dcelan2@users.noreply.github.com>
2021-01-29 00:58:08 +0100

    Added .NET Core project in Visual Studio

---
+++ BitcoinBlockexplorer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BitcoinBlockexplorer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

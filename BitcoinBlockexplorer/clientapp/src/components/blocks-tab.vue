31783a3ca7b1dc0381afd747176a60348e705cb1
dcelan2 <56230271+dcelan2@users.noreply.github.com>
2021-01-29 23:51:18 +0100

    Fetched blocks and transactions to frontend. Made data tables, needed to edit fields.

--- BitcoinBlockexplorer/clientapp/src/components/blocks-tab.vue
+++ BitcoinBlockexplorer/clientapp/src/components/blocks-tab.vue
@@ -1,4 +1,5 @@
 <script>
+import Axios from 'axios'
 
 export default {
   components: {
@@ -6,26 +7,55 @@ export default {
   },
   data() {
       return {
-        test: null
+        blocks: [],
+        fields: [ 
+          { key: 'result.hash', label: "Test", sortable: true },
+          { key: 'result.height', label: "Test", sortable: true },
+          { key: 'result.difficulty', label: "Test", sortable: true },
+          { key: 'result.size', label: "Test", sortable: true },
+          { key: 'result.time', label: "Test", sortable: true },
+          { key: 'result.weight', label: "Test", sortable: true },
+        ]
       }
   },
   created() {
-    // Axios
-    //   .get('/api/BlockChainExplorer/getresultsfromhost?queryJson=' + this.$createRequestData("getrawtransaction", ["194413584f3920fe5c97566e0d43cbc5707da4d0f121ca1f5435007181cb003b"]))
-    //   .then(response => {
-    //       console.log(response.data)
-    //       this.test = response.data
-    //   })
-    //   .catch(error => {
-    //       this.$bvToast.toast(error.response.data,
-    //           { title: this.$t('error'), variant: "danger", solid: true })
-    //   })
+    Axios
+      .get('/api/BlockChainExplorer/getresultsfromhost?queryJson=' + this.$createRequestData("getbestblockhash", []))
+      .then(response => {
+          this.getNLastBlocks(10, response.data.result)
+      })
+      .catch(error => {
+          this.$bvToast.toast(error.response.data,
+              { title: this.$t('error'), variant: "danger", solid: true })
+      })
   },
+  methods: {
+    loadMoreBlocks() {
+      var nextBlockHash = this.blocks[this.blocks.length - 1].result.previousblockhash
+      this.getNLastBlocks(10, nextBlockHash)
+    },
+    getNLastBlocks(numberOfBlocks, startingBlockHash){
+      Axios
+        .get('/api/BlockChainExplorer/getnlastblocks?numberOfBlocks=' + numberOfBlocks + '&startingBlockHash=' + startingBlockHash)
+        .then(response => {          
+            this.blocks.push(...response.data)
+        })
+    }
+  }
 }
 </script>
 
 <template>
   <div>
-    <p>this is block tab</p>
+    <b-table outlined
+              hover
+              primary-key="hash"
+              :items="blocks"
+              :fields="fields"
+              id="data-table"
+              class="mb-2"
+              style="cursor:pointer">           
+    </b-table>
+    <b-button @click="loadMoreBlocks" block variant="outline-secondary">Load More</b-button>
   </div>
 </template>
--- BitcoinBlockexplorer/clientapp/src/components/transactions-tab.vue
+++ BitcoinBlockexplorer/clientapp/src/components/transactions-tab.vue
@@ -1,4 +1,5 @@
 <script>
+import Axios from 'axios'
 
 export default {
   components: {
@@ -6,26 +7,65 @@ export default {
   },
   data() {
       return {
-        test: null
+        rawMempool: null, //transactionIds
+        mempoolEntries: [], //transactions from mempool shown on the screen
+        fields: [ 
+          { key: 'result.hash', label: "Test", sortable: true },
+          { key: 'result.height', label: "Test", sortable: true },
+          { key: 'result.difficulty', label: "Test", sortable: true },
+          { key: 'result.size', label: "Test", sortable: true },
+          { key: 'result.time', label: "Test", sortable: true },
+          { key: 'result.weight', label: "Test", sortable: true },
+        ]
       }
   },
   created() {
-    // Axios
-    //   .get('/api/BlockChainExplorer/getresultsfromhost?queryJson=' + this.$createRequestData("getrawtransaction", ["194413584f3920fe5c97566e0d43cbc5707da4d0f121ca1f5435007181cb003b"]))
-    //   .then(response => {
-    //       console.log(response.data)
-    //       this.test = response.data
-    //   })
-    //   .catch(error => {
-    //       this.$bvToast.toast(error.response.data,
-    //           { title: this.$t('error'), variant: "danger", solid: true })
-    //   })
+    Axios
+      .get('/api/BlockChainExplorer/getresultsfromhost?queryJson=' + this.$createRequestData("getrawmempool", [false]))
+      .then(response => {
+          this.rawMempool = response.data.result
+          this.getMempoolEntries(response.data.result.slice(0, 10))
+      })
+      .catch(error => {
+          this.$bvToast.toast(error.response.data,
+              { title: "Error", variant: "danger", solid: true })
+      })
   },
+  methods: {
+    loadMoreMempoolEntries() {
+      var increasement = 10
+      if ((this.mempoolEntries.length + increasement) > this.rawMempool.length){
+        increasement = this.rawMempool.length - this.mempoolEntries.length
+      }
+      if (increasement <= 0){
+         this.$bvToast.toast("There is no more data to show. Try refreshing page.",
+              { title: "Warrning", variant: "info", solid: true })
+      } else {
+        this.getMempoolEntries(this.rawMempool.slice(this.mempoolEntries.length, this.mempoolEntries.length + increasement))
+      }      
+    },
+    getMempoolEntries(transactionIds){
+      Axios
+        .post('/api/BlockChainExplorer/getmempoolentries', transactionIds)
+        .then(response => {          
+            this.mempoolEntries.push(...response.data)
+        })
+    }
+  }
 }
 </script>
 
 <template>
   <div>
-    <p>this is transactions tab</p>
+    <b-table outlined
+              hover
+              primary-key="hash"
+              :items="mempoolEntries"
+              :fields="fields"
+              id="data-table"
+              class="mb-2"
+              style="cursor:pointer">           
+    </b-table>
+    <b-button @click="loadMoreMempoolEntries" block variant="outline-secondary">Load More</b-button>
   </div>
 </template>

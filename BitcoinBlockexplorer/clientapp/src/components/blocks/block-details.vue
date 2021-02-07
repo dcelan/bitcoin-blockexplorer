ffe7ec1fa8df04b20d1e13c542279c498e9c0ec2
dcelan2 <56230271+dcelan2@users.noreply.github.com>
2021-02-07 01:07:29 +0100

    Fixed block transactions list update when selecting previous block link

--- BitcoinBlockexplorer/clientapp/src/components/blocks/block-details.vue
+++ BitcoinBlockexplorer/clientapp/src/components/blocks/block-details.vue
@@ -36,12 +36,14 @@ export default {
   methods: {
     changeBlockDetails(blockHash){
       this.loading = true
-        Axios
-          .get('/api/BlockChainExplorer/getblock?blockHash=' + blockHash)
-          .then(response => {
-              this.blockDetails = response.data
-              this.getBlockStatistics(response.data.result.height)
-          })
+      this.blockDetails = null
+      this.blockStatistics = null
+      Axios
+        .get('/api/BlockChainExplorer/getblock?blockHash=' + blockHash)
+        .then(response => {
+            this.blockDetails = response.data
+            this.getBlockStatistics(response.data.result.height)
+        })
     },
     getBlockStatistics(blockHeight){
       Axios
@@ -59,41 +61,42 @@ export default {
 
 <template>
   <b-overlay :show="loading" variant="white">
-    
-    <div v-if="!search" class="text-right">
-      <b-button variant="outline-secondary" @click="$emit('back')"> Back</b-button>
-    </div>
 
-    <h5 class="card-title text-info">Bitcoin block <small class="text-muted"
-        style="font-size:1.5rem">{{blockDetails.result.height}}</small></h5>         
+    <div v-if="blockDetails && blockStatistics">
+      <div v-if="!search" class="text-right">
+        <b-button variant="outline-secondary" @click="$emit('back')"> Back</b-button>
+      </div>
 
-    <div class="border-top-0" style="margin-top:-1px">
-      <div class="card-body p-1">
-        <b-row>
-          <b-col lg="12" class="p-1">
-            <BlockDetailsLinks :blockDetails="blockDetails" :blockStatistics="blockStatistics" @change-block-details="changeBlockDetails" />
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col lg="12" class="p-1">
-            <BlockDetailsSimpleData :blockDetails="blockDetails" :blockStatistics="blockStatistics" />
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col lg="6" class="p-1">
-            <BlockDetailsFees :blockDetails="blockDetails" :blockStatistics="blockStatistics" />
-          </b-col>
-          <b-col lg="6" class="p-1">
-            <BlockDetailsFeeRates :blockDetails="blockDetails" :blockStatistics="blockStatistics" />
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col lg="12" class="p-1">
-            <BlockTransactions :transactionsIds="blockDetails.result.tx" />
-          </b-col>
-        </b-row>
+      <h5 class="card-title text-info">Bitcoin block <small class="text-muted"
+          style="font-size:1.5rem">{{blockDetails.result.height}}</small></h5>         
+
+      <div class="border-top-0" style="margin-top:-1px">
+        <div class="card-body p-1">
+          <b-row>
+            <b-col lg="12" class="p-1">
+              <BlockDetailsLinks :blockDetails="blockDetails" :blockStatistics="blockStatistics" @change-block-details="changeBlockDetails" />
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col lg="12" class="p-1">
+              <BlockDetailsSimpleData :blockDetails="blockDetails" :blockStatistics="blockStatistics" />
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col lg="6" class="p-1">
+              <BlockDetailsFees :blockDetails="blockDetails" :blockStatistics="blockStatistics" />
+            </b-col>
+            <b-col lg="6" class="p-1">
+              <BlockDetailsFeeRates :blockDetails="blockDetails" :blockStatistics="blockStatistics" />
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col lg="12" class="p-1">
+              <BlockTransactions :transactionsIds="blockDetails.result.tx" />
+            </b-col>
+          </b-row>
+        </div>
       </div>
     </div>
-
   </b-overlay>
 </template>
